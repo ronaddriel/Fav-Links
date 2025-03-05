35985508d13eeafe768894e786ac3462ffbe7aa6
Ron <ronaddriel@Rons-MBP.lehman.edu>
2025-03-05 16:26:04 -0500

    Commit Troubleshooting lol

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,8 @@
 import { useState } from "react" 
 import Form from "./components/Form"
 import Table from "./components/Table"
+
+
 function HomePage(){
     return (
         <div>
@@ -12,7 +14,7 @@ function HomePage(){
            {/*Table to display the data*/}
 
            <Table />
-            
+
         </div>   
     )
 }
